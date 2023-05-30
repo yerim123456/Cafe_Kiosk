befc68d72c523d10568585114d6edae8e5258663
dlaywls <gywls5184@naver.com>
2023-05-30 18:28:01 +0900

    payment1

---
+++ Cafe_Kiosk/resource.h
@@ -0,0 +1,66 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// CafeKiosk.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_CafeKioskTYPE               130
+#define ID_WINDOW_MANAGER               131
+#define IDD_DIALOG1                     310
+#define IDD_DIALOG2                     312
+#define IDD_DIALOG3                     313
+#define IDD_DIALOG4                     314
+#define IDD_DIALOG5                     315
+#define IDD_DIALOG6                     316
+#define IDD_DIALOG9                     317
+#define IDD_DIALOG8                     319
+#define IDD_HOME                        319
+#define IDD_PAY1                        321
+#define IDD_PAY2                        323
+#define IDC_TAB2                        1042
+#define IDC_LIST2                       1046
+#define IDC_LIST7                       1051
+#define IDC_EDIT3                       1054
+#define IDC_BUTTON14                    1055
+#define IDC_BUTTON17                    1062
+#define IDC_BUTTON18                    1063
+#define IDC_BUTTON19                    1064
+#define IDC_                            1064
+#define IDC_BUTTON_EATIN                1064
+#define IDC_EDIT1                       1066
+#define IDC_EDIT_MONEY                  1067
+#define IDC_EDIT_ORDER                  1068
+#define IDC_CHECK2                      1069
+#define IDC_RADIO2                      1070
+#define IDC_RADIO3                      1071
+#define IDC_BUTTON1                     1072
+#define IDC_RADIO4                      1074
+#define IDC_BUTTON_EASYPAY              1074
+#define IDC_BUTTON_PAYMENT              1075
+#define IDC_BUTTON_BACK                 1076
+#define IDC_EDIT2                       1077
+#define IDC_EDIT_QUANTITY               1078
+#define IDC_EDIT_PAYMENT                1079
+#define IDC_EDIT_OERDERPAY              1080
+#define IDC_EDIT4                       1081
+#define IDC_EDIT_POINT                  1081
+#define IDC_CHECK3                      1082
+#define IDC_EDIT_NUMBER                 1082
+#define IDC_CHECK4                      1083
+#define IDC_EDIT_SAVEPOINT              1083
+#define IDC_BUTTON2                     1084
+#define IDC_CHECK_PRINT                 1084
+#define IDC_BUTTON_HOME                 1085
+#define IDC_BUTTON_TAKEOUT              1086
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        325
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1087
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
